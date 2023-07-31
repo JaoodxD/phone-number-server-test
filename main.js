addfe435b832da915f8ae60c4ed3705aeaf5759d
JaoodxD <jaood96@gmail.com>
2023-07-31 22:22:21 +0300

    Refactor server routing with strategy pattern

--- main.js
+++ main.js
@@ -19,25 +19,30 @@ const data = await getConfig(DATA_URL)
 const utils = phoneNumberInitializer(data)
 const html = createHTML(SERVER_ADDRESS)
 
+const routes = {
+  'static': () => html,
+  'libExec': (query) => {
+    const decoded = decodeURIComponent(query)
+    const { phone, ISO } = parseParams(decoded)
+    if (!phone) return 'No phone provided'
+    if (!ISO) return 'No ISO country provided'
+    try {
+      const operator = utils.recognizeOperator(phone, ISO)
+      const formattedPhone = utils.formatPhone(phone, ISO)
+      const response = `${operator}: ${formattedPhone}`
+      return response
+    } catch ({ message }) {
+      return message
+    }
+  }
+}
+
 const server = createServer((req, res) => {
   const { url } = req
+  console.log({ url })
   const query = url.slice(2)
-  if (query === '') {
-    return res.end(html)
-  }
-  const decoded = decodeURIComponent(query)
-  const { phone, ISO } = parseParams(decoded)
-  console.log({ url, query, phone, ISO })
-  if (!phone) return res.end('No phone provided')
-  if (!ISO) return res.end('No ISO country provided')
-  let response;
-  try {
-    const operator = utils.recognizeOperator(phone, ISO)
-    const formattedPhone = utils.formatPhone(phone, ISO)
-    response = `${operator}: ${formattedPhone}`
-  } catch ({ message }) {
-    response = message
-  }
+  const handler = query === '' ? routes.static : routes.libExec
+  const response = handler(query);
   res.end(response)
 })
 
