e60ecad3ce710b625862ba0cca28a13046972bf9
JaoodxD <jaood96@gmail.com>
2023-07-31 21:06:24 +0300

    Add template to create static html with specified server url

---
+++ static.js
@@ -0,0 +1,43 @@
+export default function createHTML(url) {
+  return `<!DOCTYPE html>
+  <html lang="en">
+  
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+  </head>
+  
+  <body>
+    <input type="text" name="phone" id="phone" alt="phone">
+    <input type="text" name="country" id="country" alt="country">
+    <label id="output"></label>
+    <script>
+      const phoneInput = document.querySelector('#phone')
+      const countryInput = document.querySelector('#country')
+      const outputLabel = document.querySelector('#output')
+      const url = '${url}'
+      const debounced = debounce(makeRequest, 500)
+      phoneInput.addEventListener('input', debounced)
+      countryInput.addEventListener('input', debounced)
+  
+      function debounce(func, timeout = 300) {
+        let timer;
+        return (...args) => {
+          clearTimeout(timer)
+          timer = setTimeout(() => { func.apply(this, args); }, timeout)
+        }
+      }
+      async function makeRequest() {
+        const phone = phoneInput.value
+        const country = countryInput.value
+        const request = \`\${url} /?phone=\${phone}&ISO=\${country}\`
+  const result = await fetch(request).then((data) => data.text())
+  outputLabel.textContent = result
+}
+    </script >
+  </body >
+  
+  </html >
+  `
+}
